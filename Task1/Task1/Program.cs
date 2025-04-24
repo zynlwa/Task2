14fc761745fead84bbe31a7c9378ed8e6a845499
Shefa <zeyna@example.com>
2025-04-24 14:43:10 +0400

    added Task1

---
+++ Task1/Task1/Program.cs
@@ -0,0 +1,36 @@
+﻿//int a = 9;
+//if (a == 0)
+//{
+//   Console.WriteLine("Duzgun eded daxil edin");
+
+//}else if(a % 3 == 0){
+//    Console.WriteLine("Eded 3-e bolunur");
+//}
+//else
+//{
+//    Console.WriteLine("Eded 3-e bolunmur");
+
+//}
+//tap2
+//int a = 16;
+//if (a >10 && a < 15)
+//{
+//    Console.WriteLine("True");
+//}
+//else
+//{
+//    Console.WriteLine("False");
+//}
+//tap3
+//int a = 23;
+//if (a == 0)
+//{
+//    Console.WriteLine("Duzgun eded daxil edin");
+//}else if (a%3 == 0 && a % 7 == 0)
+//{
+//    Console.WriteLine("Eded 3-e ve 7-e bolunur");
+//}
+//else
+//{
+//    Console.WriteLine("Eded 3 - e ve 7 - e bolunmur");
+//}
